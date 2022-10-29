cd38290ed4202d0eda59f521a70cb0b55d51ffa9
Woodie-Shi <woodie.shichenglu@foxmail.com>
2022-10-29 18:04:32 +0800

    init

---
+++ Lab2/Code/main.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "syntaxTree.h"
+
+extern int yylineno;
+extern FILE* yyin;
+extern void yyrestart(FILE *);
+extern int yyparse();
+extern int Error;
+
+int main(int argc, char** argv)
+{
+    if (argc <= 1) return 1;
+    FILE* f = fopen(argv[1], "r");
+    if (!f){
+        perror(argv[1]);
+        return 1;
+    }
+    yylineno = 1;
+    yyrestart(f);
+    yyparse();
+    if(!Error) tree_display(treeRoot, 0);
+    return 0;
+}
+/*int main(int argc, char** argv) {
+    if (argc > 1) {
+        if (!(yyin = fopen(argv[1], "r"))) {
+            perror(argv[1]);
+            return 1;
+        }
+    }
+    while (yylex() != 0);
+    return 0;
+}*/
---
+++ Lab2/Code/syntaxTree.h
@@ -0,0 +1,29 @@
+#ifndef _SYNTAXTREE_H
+#define _SYNTAXTREE_H
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+extern char *yytext;
+extern int yyleng;
+extern int yylineno;
+
+enum syntax_type {lexical, syntactic};
+typedef struct TreeNode{
+    char* name;
+    union{
+        int val_int;
+        float val_float;
+        char* val_str;
+    };
+    int children_num;
+    struct TreeNode** children;
+    int lineno;
+    enum syntax_type type;
+}TreeNode;
+
+TreeNode* node_init(char* _name, enum syntax_type _type);
+void node_insert(int num, TreeNode*parent,  TreeNode*_children[]);
+void tree_display(TreeNode* root, int depth);
+TreeNode* treeRoot;
+#endif
